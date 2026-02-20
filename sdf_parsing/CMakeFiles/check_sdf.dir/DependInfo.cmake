398d106fd3b5a1b17c893ec5a4ce7e82767c890d
dokyun-kim4 <donny5557@gmail.com>
2026-02-20 19:30:52 +0000

    add required files for slung sim

---
+++ sdf_parsing/CMakeFiles/check_sdf.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/soil_sampling_master/repos/PX4-Autopilot/Tools/simulation/gz/sdf_parsing/check_sdf.cc" "CMakeFiles/check_sdf.dir/check_sdf.cc.o" "gcc" "CMakeFiles/check_sdf.dir/check_sdf.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sdf_parsing/CMakeFiles/check_sdf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check_sdf.dir/check_sdf.cc.o"
+  "CMakeFiles/check_sdf.dir/check_sdf.cc.o.d"
+  "check_sdf"
+  "check_sdf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check_sdf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
